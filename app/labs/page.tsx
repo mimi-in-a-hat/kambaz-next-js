35e35617cb91ba5b53c199316028888946d12385
mimi <zhu.emily@northeastern.edu>
2026-02-02 20:17:47 -0500

    e

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -4,5 +4,6 @@ export default function Kambaz() {
       <h1>Labs</h1>
       <h2>Emily Zhu</h2>
       <h2>VTL-1-OL</h2>
+      <h2>https://github.com/mimi-in-a-hat/kambaz-next-js</h2>
     </div>
 );}
