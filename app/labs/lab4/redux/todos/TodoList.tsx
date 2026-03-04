902d1f47ca43aff2a2a90121e518f82a03ad1ab8
mimi <zhu.emily@northeastern.edu>
2026-03-04 18:52:27 -0500

    stopped being stupid

--- app/labs/lab4/redux/todos/TodoList.tsx
+++ app/labs/lab4/redux/todos/TodoList.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../store";
 import { ListGroup } from "react-bootstrap";
+import { deleteTodo, setTodo } from "./todosReducer";
+
 export default function TodoList() {
   const { todos } = useSelector((state: RootState) => state.todosReducer);
+  const dispatch = useDispatch();
+  
   return (
     <div id="wd-todo-list-redux">
       <h2>Todo List</h2>
       <ListGroup>
         <TodoForm />
-        {todos.map((todo: any) => (
-          <TodoItem key={todo.id} todo={todo} />
+        {todos.map((todo: { id: string; title: string }) => (
+          <TodoItem 
+            key={todo.id} 
+            todo={todo}
+            deleteTodo={(id: string) => dispatch(deleteTodo(id))}
+            setTodo={(todo: { id: string; title: string }) => dispatch(setTodo(todo))}
+          />
         ))}
       </ListGroup>
       <hr/>
