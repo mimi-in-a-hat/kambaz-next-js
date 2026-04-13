39ce9b023d196948fdb2a79bd6e465b5cd70c0ac
mimi <zhu.emily@northeastern.edu>
2026-04-12 23:01:06 -0400

    e

--- app/(kambaz)/account/reducer.ts
+++ app/(kambaz)/account/reducer.ts
@@ -1,12 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type User = {
+  _id?: string;
+  username?: string;
+  role: string;
+  [key: string]: unknown;
+};
+
+type AccountState = {
+  currentUser: User | null;
+};
+
+const initialState: AccountState = {
   currentUser: null,
 };
 const accountSlice = createSlice({
   name: "account",
   initialState,
   reducers: {
-    setCurrentUser: (state, action) => {
+    setCurrentUser: (state, action: PayloadAction<User | null>) => {
       state.currentUser = action.payload;
     },
   },
